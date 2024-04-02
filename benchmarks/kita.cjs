bf1674d0bdfbbcf5808755f512d1c07a28fc753a
Arthur Fiorette <me@arthur.place>
2024-04-02 10:56:23 -0300

    Added kita to benchmarks

---
+++ benchmarks/kita.cjs
@@ -0,0 +1,17 @@
+'use strict';
+
+const path = require('path');
+
+globalThis.KITA_PROJECT_ROOT = path.resolve(__dirname, '../kita/dist');
+
+const { Kita } = require('@kitajs/runtime');
+const fastify = require('fastify');
+
+const app = fastify();
+
+app.register(Kita, {
+  fastifyScalarUi: false,
+  fastifySwagger: false
+});
+
+app.listen({ port: 3000, host: '127.0.0.1' });
---
+++ kita/kita.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+/** @type {import('@kitajs/cli').KitaConfig} */
+module.exports = {
+  tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+  src: path.resolve(__dirname, 'src'),
+  cwd: path.resolve(__dirname)
+};
