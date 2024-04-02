06f9fae8fa30bbd2a341fe993f93ce67e3e664ae
Arthur Fiorette <me@arthur.place>
2024-04-02 12:21:48 -0300

    Fix lint

--- benchmarks/kita.cjs
+++ benchmarks/kita.cjs
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
-const path = require('path');
+const path = require('path')
 
-globalThis.KITA_PROJECT_ROOT = path.resolve(__dirname, '../kita/dist');
+globalThis.KITA_PROJECT_ROOT = path.resolve(__dirname, '../kita/dist')
 
-const { Kita } = require('@kitajs/runtime');
-const fastify = require('fastify');
+const { Kita } = require('@kitajs/runtime')
+const fastify = require('fastify')
 
-const app = fastify();
+const app = fastify()
 
 app.register(Kita, {
   fastifyScalarUi: false,
   fastifySwagger: false
-});
+})
 
-app.listen({ port: 3000, host: '127.0.0.1' });
+app.listen({ port: 3000, host: '127.0.0.1' })
